814385a301bd0d16fd5166473c96b0e457bfc959
Space Rabbit Maco <Space.Rabbit.Maco@gmail.com>
2018-12-12 20:06:29 +0300

    Fix  'CURLOPT_SSL_VERIFYHOST' and 'CURLOPT_SSL_VERIFYPEER' to '2' in cron-get-data-cmc-usd.php

--- cron/cron-get-data-cmc-usd.php
+++ cron/cron-get-data-cmc-usd.php
@@ -57,8 +57,8 @@ function getUSDRates() {
 	curl_setopt($ch, CURLOPT_URL, 'https://api.exchangeratesapi.io/latest?base=USD');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, true);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 2);
 	$data = curl_exec($ch);
 	curl_close($ch);
 
